adbad6980b0165b0d9321f72373b1e0de700ce82
Evan Henderson <adamhenderson3x3@gmail.com>
2020-04-03 01:29:52 +0700

    Add additional resource

--- client/src/client/Index.jsx
+++ client/src/client/Index.jsx
@@ -1,5 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Appointment from '../components/Appointment';
+import Picture from '../res/builder.jpg';
+import Picture2 from '../res/builder2.jpg';
+import Picture3 from '../res/builder3.jpg';
 export default class Index extends Component {
 	constructor() {
 		super();
@@ -13,21 +16,24 @@ export default class Index extends Component {
 	handleModalStatus() {
 		this.setState(() => ({ modalStatus: !this.state.modalStatus }));
 	}
+
 	render() {
 		return (
-			<div className='jumbotron animated bounce infinite'>
-				<h1 className='display-3'>We are here to assist you</h1>
-				<p className='lead'>This site created by Evan Henderson</p>
-				<hr className='my-2' />
-				<button
-					disabled={!localStorage.getItem('_id')}
-					onClick={this.handleModalStatus}
-					className='btn btn-outline-primary'
-				>
-					Make an appointment
-				</button>
-				<Appointment modalStatus={this.state.modalStatus} handleModalStatus={this.handleModalStatus} />
-			</div>
+			<Fragment>
+				<div className='jumbotron animated bounce infinite'>
+					<h1 className='display-3'>We are here to assist you</h1>
+					<p className='lead'>This site created by Evan Henderson</p>
+					<hr className='my-2' />
+					<button
+						disabled={!localStorage.getItem('_id')}
+						onClick={this.handleModalStatus}
+						className='btn btn-outline-primary'
+					>
+						Make an appointment
+					</button>
+					<Appointment modalStatus={this.state.modalStatus} handleModalStatus={this.handleModalStatus} />
+				</div>
+			</Fragment>
 		);
 	}
 }
