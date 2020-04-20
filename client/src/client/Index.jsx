cb2d1e9a63327c7b493475ddb9b98616e9ff99f7
EvanHendersonRichtByte <adamhenderson3x3@gmail.com>
2020-04-20 20:08:12 +0700

    Update dependency and index UI

--- client/src/client/Index.jsx
+++ client/src/client/Index.jsx
@@ -1,39 +1,112 @@
-import React, { Component, Fragment } from 'react';
-import Appointment from '../components/Appointment';
-import Picture from '../res/builder.jpg';
-import Picture2 from '../res/builder2.jpg';
-import Picture3 from '../res/builder3.jpg';
+import React, { Component, Fragment } from "react";
+import Appointment from "../components/Appointment";
+import "../style/style.css";
+import Picture1 from "../res/builder.jpg";
+import Picture2 from "../res/builder2.jpg";
+import Picture3 from "../res/builder3.jpg";
+
 export default class Index extends Component {
-	constructor() {
-		super();
-		this.handleModalStatus = this.handleModalStatus.bind(this);
-		this.state = {
-			modalStatus: false,
-			_id: ''
-		};
-	}
+  constructor() {
+    super();
+    this.handleModalStatus = this.handleModalStatus.bind(this);
+    this.state = {
+      modalStatus: false,
+      _id: "",
+    };
+  }
 
-	handleModalStatus() {
-		this.setState(() => ({ modalStatus: !this.state.modalStatus }));
-	}
+  handleModalStatus() {
+    this.setState(() => ({ modalStatus: !this.state.modalStatus }));
+  }
 
-	render() {
-		return (
-			<Fragment>
-				<div className='jumbotron animated bounce infinite'>
-					<h1 className='display-3'>We are here to assist you</h1>
-					<p className='lead'>This site created by Evan Henderson</p>
-					<hr className='my-2' />
-					<button
-						disabled={!localStorage.getItem('_id')}
-						onClick={this.handleModalStatus}
-						className='btn btn-outline-primary'
-					>
-						Make an appointment
-					</button>
-					<Appointment modalStatus={this.state.modalStatus} handleModalStatus={this.handleModalStatus} />
-				</div>
-			</Fragment>
-		);
-	}
+  render() {
+    return (
+      <Fragment>
+        <div className="col-md-12" id="header">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="container">
+                <div className="container">
+                  <div className="container">
+                    <center className="text-light" id="index-text-center">
+                      <h6>Building world for the better future</h6>
+                      <h3>All things need to repair</h3>
+                      <p>
+                        Consequatur quam culpa et consequatur beatae. Est libero
+                        occaecati. Harum laboriosam consectetur voluptates
+                        excepturi incidunt aspernatur quia voluptas.
+                      </p>
+                      <button
+                        disabled={!localStorage.getItem("_id")}
+                        onClick={this.handleModalStatus}
+                        className="btn btn-light"
+                      >
+                        Make an appointment
+                      </button>
+                      <Appointment
+                        modalStatus={this.state.modalStatus}
+                        handleModalStatus={this.handleModalStatus}
+                      />
+                    </center>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          className="col-md-12"
+          style={{ marginTop: "40px"}}
+        >
+          <h3 className="text-center" style={{marginBottom: "40px"}}>Our Services</h3>
+          <div className="row">
+            <div className="col-md-3">
+              <div className="card" style={{ width: "18rem" }}>
+                <img className="card-img-top" src={Picture3} alt="oawkdoakwdok" />
+                <div className="card-body">
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card" style={{ width: "18rem" }}>
+                <img className="card-img-top" src={Picture1} alt="oawkdoakwdok" />
+                <div className="card-body">
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card" style={{ width: "18rem" }}>
+                <img className="card-img-top" src={Picture2} alt="oawkdoakwdok" />
+                <div className="card-body">
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card" style={{ width: "18rem" }}>
+                <img className="card-img-top" src={Picture3} alt="oawkdoakwdok" />
+                <div className="card-body">
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,143 +1,189 @@
-import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
-import Logo from '../res/eagle.png';
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+  FaInstagram,
+} from "react-icons/fa";
 const Navbar = () => {
-	const isLogged = () => {
-		let role = localStorage.getItem('role');
-		let status = localStorage.getItem('status');
-		role = JSON.parse(role);
-		if (role && status) {
-			if (role === 'Admin') {
-				return (
-					<Fragment>
-						<li className='nav-item'>
-							<Link to='/dashboard' className='nav-link text-danger'>
-								Dashboard
-							</Link>
-						</li>
-						<li className='nav-item'>
-							<Link to='/sparepart' className='nav-link text-info'>
-								Sparepart Table Data
-							</Link>
-						</li>
-						<li className='nav-item'>
-							<Link to='/employee-acceptance' className='nav-link text-success'>
-								Employee Registrations
-							</Link>
-						</li>
-						<li className='nav-item'>
-							<Link
-								onClick={() => {
-									window.location.assign('/login');
-									localStorage.clear();
-								}}
-								to='/login'
-								className='nav-link'
-							>
-								Logout
-							</Link>
-						</li>
-					</Fragment>
-				);
-			} else if (role === 'Employee') {
-				return (
-					<Fragment>
-						<li>
-							<Link to='/profile' className='nav-link'>
-								Profile
-							</Link>
-						</li>
-						<li className='nav-item'>
-							<Link
-								onClick={() => {
-									window.location.assign('/login');
-									localStorage.clear();
-								}}
-								to='/login'
-								className='nav-link'
-							>
-								Logout
-							</Link>
-						</li>
-					</Fragment>
-				);
-			} else {
-				return (
-					<Fragment>
-						<li>
-							<Link to='/profile' className='nav-link'>
-								Profile
-							</Link>
-						</li>
-						<li>
-							<Link to='/issue-list' className='nav-link'>
-								My Issues
-							</Link>
-						</li>
-						<li className='nav-item'>
-							<Link
-								onClick={() => {
-									window.location.assign('/login');
-									localStorage.clear();
-								}}
-								to='/login'
-								className='nav-link'
-							>
-								Logout
-							</Link>
-						</li>
-					</Fragment>
-				);
-			}
-		} else {
-			return (
-				<Fragment>
-					<li className='nav-item'>
-						<Link to='/login' className='nav-link'>
-							Login
-						</Link>
-					</li>
-					<li>
-						<Link to='/register' className='nav-link'>
-							Register
-						</Link>
-					</li>
-				</Fragment>
-			);
-		}
-	};
-	return (
-		<nav className='navbar navbar-expand-sm navbar-light bg-light'>
-			<Link to='/' className='navbar-brand'>
-				<div className='d-flex'>
-					<img style={{ height: '30px', width: 'auto' }} src={Logo} alt='corp logo' />
-					<h5>
-						Krypto<span className='text-danger'>Force</span>
-					</h5>
-				</div>
-			</Link>
-			<button
-				className='navbar-toggler d-lg-none'
-				type='button'
-				data-toggle='collapse'
-				data-target='#collapsibleNavId'
-				aria-controls='collapsibleNavId'
-				aria-expanded='false'
-				aria-label='Toggle navigation'
-			>
-				<span className='navbar-toggler-icon' />
-			</button>
-			<div className='collapse navbar-collapse' id='collapsibleNavId'>
-				<ul className='navbar-nav mr-auto mt-2 mt-lg-0'>{isLogged()}</ul>
-				<form className='form-inline my-2 my-lg-0'>
-					<input className='form-control mr-sm-2' type='text' placeholder='Search' />
-					<button className='btn btn-outline-success my-2 my-sm-0' type='submit'>
-						Search
-					</button>
-				</form>
-			</div>
-		</nav>
-	);
+  const isLogged = () => {
+    let role = localStorage.getItem("role");
+    let status = localStorage.getItem("status");
+    role = JSON.parse(role);
+    if (role && status) {
+      if (role === "Admin") {
+        return (
+          <Fragment>
+            <li className="nav-item">
+              <Link to="/dashboard" className="nav-link text-danger">
+                Dashboard
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/sparepart" className="nav-link text-info">
+                Sparepart Table Data
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/employee-acceptance" className="nav-link text-success">
+                Employee Registrations
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                onClick={() => {
+                  window.location.assign("/login");
+                  localStorage.clear();
+                }}
+                to="/login"
+                className="nav-link"
+              >
+                Logout
+              </Link>
+            </li>
+          </Fragment>
+        );
+      } else if (role === "Employee") {
+        return (
+          <Fragment>
+            <li>
+              <Link to="/profile" className="nav-link">
+                Profile
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                onClick={() => {
+                  window.location.assign("/login");
+                  localStorage.clear();
+                }}
+                to="/login"
+                className="nav-link"
+              >
+                Logout
+              </Link>
+            </li>
+          </Fragment>
+        );
+      } else {
+        return (
+          <Fragment>
+            <li>
+              <Link to="/profile" className="nav-link">
+                Profile
+              </Link>
+            </li>
+            <li>
+              <Link to="/issue-list" className="nav-link">
+                My Issues
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                onClick={() => {
+                  window.location.assign("/login");
+                  localStorage.clear();
+                }}
+                to="/login"
+                className="nav-link"
+              >
+                Logout
+              </Link>
+            </li>
+          </Fragment>
+        );
+      }
+    } else {
+      return (
+        <Fragment>
+          <li className="nav-item">
+            <Link to="/login" className="nav-link">
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link to="/register" className="nav-link">
+              Register
+            </Link>
+          </li>
+        </Fragment>
+      );
+    }
+  };
+  return (
+    <Fragment>
+      <nav className="navbar navbar-expand navbar-dark bg-dark text-light" style={{height: "40px"}}>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <span className="nav-link" id="kr-nav">
+              <FaFacebookF />
+            </span>
+          </li>
+          <li className="nav-item">
+            <span className="nav-link" id="kr-nav">
+              <FaTwitter />
+            </span>
+          </li>
+          <li className="nav-item">
+            <span className="nav-link" id="kr-nav">
+              <FaLinkedinIn />
+            </span>
+          </li>
+          <li className="nav-item">
+            <span className="nav-link" id="kr-nav">
+              <FaInstagram />
+            </span>
+          </li>
+        </ul>
+        <div className="col-md-11 d-flex justify-content-end">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <span className="nav-link" id="kr-nav">
+                + 666 666 666
+              </span>
+            </li>
+            <li className="nav-item">
+              <span className="nav-link" id="kr-nav">
+                kryptonforce@gmail.com
+              </span>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <nav className="navbar navbar-expand-sm navbar-light bg-light">
+        <Link to="/" className="navbar-brand">
+          <Link to="/" id="nav-title" className="nav-link text-dark">
+            krypto<span className="text-danger">force</span>
+          </Link>
+        </Link>
+        <button
+          className="navbar-toggler d-lg-none"
+          type="button"
+          data-toggle="collapse"
+          data-target="#collapsibleNavId"
+          aria-controls="collapsibleNavId"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="collapsibleNavId">
+          <ul
+            className="navbar-nav mr-auto mt-2 mt-lg-0"
+            style={{ marginLeft: "77%" }}
+          >
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            {isLogged()}
+          </ul>
+        </div>
+      </nav>
+    </Fragment>
+  );
 };
 
 export default Navbar;
